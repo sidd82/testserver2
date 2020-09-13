2802b2af61721e534430c21bb605e25535c07868
sidd82 <psiddhesh14@gmail.com>
2020-09-13 18:52:29 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Server2",
+  });
+});
+
+app.listen(5001, () => console.log("Server running on port 5001"));
